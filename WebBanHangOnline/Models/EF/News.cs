2cd731c237a3974e3ff12faa26e6e021166bb886
alexnguyen2003 <nguyenbahao28102003@gmail.com>
2023-09-10 05:59:39 +0700

    Add project files.

---
+++ WebBanHangOnline/Models/EF/News.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_News")]
+    public class News :CommonAbstract
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Detail { get; set; }
+        public string Image { get; set; }
+        public int  CategoryID { get; set; }
+        public string SeoTitle { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoKeywords { get; set; }
+        public virtual Category Category { get; set; } 
+
+    }
+}
---
+++ WebBanHangOnline/Models/EF/ProductImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_ProductImage")]
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string ProductID { get; set; }
+        public string Image { get; set; }
+        public string IsDefault { get; set; }
+    }
+}
